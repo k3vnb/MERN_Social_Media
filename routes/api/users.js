15c2a062f4b4f31a77e26b0ca2c7cac3fe080616
Kevin Boyle <papershack@gmail.com>
2019-03-02 22:03:39 -0800

    add logic for login and hashing passwords

--- routes/api/users.js
+++ routes/api/users.js
@@ -45,4 +45,32 @@ router.post('/register', (req, res) => {
     })
 });
 
+// @route    GET api/users/login
+// @desc     Login User /Returning JWT Token
+// @access   Public
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Find User by Email
+    User.findOne({email})
+        .then(user => {
+            // Check for user
+            if(!user){
+                return res.status(404).json({email: "User not found"});
+            }
+
+            // Check password -- user types plaintext but DB stores its hash.
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch){
+                        res.json({ msg: 'Success'})
+                    } else {
+                        return res.status(400).json({password: 'Password incorrect'})
+                    }
+                })
+
+        });
+});
+
 module.exports = router;
