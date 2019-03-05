38ca3d8e89e2d16d1f28d42bcd7054b2be0cc92f
Kevin Boyle <papershack@gmail.com>
2019-03-05 15:52:01 -0800

    add serverside validation for register user

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+//load Input Validation
+const validateRegisterInput = require('../../validation/register')
+
 // Load User model, allows any Mongoose methods the route has
 const User = require('./../models/User');
 
@@ -18,9 +21,17 @@ router.get('/test', (req, res) => res.json({msg: "Users Works"}));
 // @desc     Register
 // @access   Public
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    //check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     User.findOne({ email: req.body.email }).then(user => {
         if(user) {
-            return res.status(400).json({email: 'Email already exists'});
+            errors.email = 'Email already exists';
+            return res.status(400).json(errors);
         } else {
             const avatar = gravatar.url(req.body.email, {
                 s: '200', //size
@@ -95,7 +106,11 @@ router.get(
     '/current', 
     passport.authenticate('jwt', { session: false }), 
     (req, res) => {
-    res.json(req.user); 
+    res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+    }); 
     }
 )
 
