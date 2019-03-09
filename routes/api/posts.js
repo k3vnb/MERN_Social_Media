1d7131de01648bd2c6e9461baeaa46fddd7f64ec
Kevin Boyle <papershack@gmail.com>
2019-03-08 18:09:48 -0800

    add route for unliking a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -98,5 +98,32 @@ router.post('/like/:id', passport.authenticate('jwt', { session: false }), (req,
         })
 })
 
+// @route    POST api/posts/unlike/:id
+// @desc     like post
+// @access   PRIVATE
+router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            Post.findById(req.params.id)
+                .then(post => {
+                    // Check if user has already 'liked' this comment
+                  if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0){
+                    return res.status(400).json({ notliked: 'You have not yet liked this post' });
+                  }  
+                    // Get remove index
+                    const removeIndex = post.likes
+                        .map(item => item.user.toString())
+                        .indexOf(req.user.id);
+
+                    // Splice out of array
+                    post.likes.splice(removeIndex, 1);
+
+                    // Save
+                    post.save().then((post => res.json(post)));
+                })
+                .catch(err => res.status(404).json({ postnotfound: 'No post found'}));
+        })
+})
+
 
 module.exports = router;
