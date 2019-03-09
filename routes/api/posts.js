7e3248008dac883854f75b191b6841f896dc228d
Kevin Boyle <papershack@gmail.com>
2019-03-08 18:17:41 -0800

    create route for POSTing comment to a user post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -125,5 +125,34 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (re
         })
 })
 
+// @route    POST api/posts/comment/:id
+// @desc     add comment to post
+// @access   PRIVATE
+router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    //Check Validation
+    if (!isValid){
+        // If any errors, send 400 w/ errors object
+        return res.status(400).json(errors)
+    }
+    
+    Post.findById(req.params.id)
+        .then(post => {
+            const newComment = {
+                text: req.body.text,
+                name: req.body.name,
+                avatar: req.body.avatar,
+                user: req.user.id
+            }
+
+            // Add to comments array
+            post.comments.unshift(newComment);
+
+            //Save
+            post.save().then(post => res.json(post))
+        })
+        .catch(err => res.status(404).json({ postnowfound: 'No post found'}));
+});
 
 module.exports = router;
